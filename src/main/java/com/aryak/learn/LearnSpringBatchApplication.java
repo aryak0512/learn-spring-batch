752c9f8b76853179afdb6e8c5efd56a6bc91f824
Aryak Deshpande <aryak.deshpande0512@gmail.com>
2025-09-01 13:47:53 +0530

    Initial commit

---
+++ src/main/java/com/aryak/learn/LearnSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.aryak.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringBatchApplication.class, args);
+	}
+
+}
