eb69717215e6d7631f98e62842839e4964d71fc2
Aryak Deshpande <aryak.deshpande0512@gmail.com>
2025-09-01 14:47:29 +0530

    Launching job via command-line runner

--- src/main/java/com/aryak/learn/LearnSpringBatchApplication.java
+++ src/main/java/com/aryak/learn/LearnSpringBatchApplication.java
@@ -1,13 +1,40 @@
 package com.aryak.learn;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LearnSpringBatchApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LearnSpringBatchApplication.class, args);
-	}
+    private final Job job;
+    private final JobLauncher jobLauncher;
+
+    public LearnSpringBatchApplication(final Job job, final JobLauncher jobLauncher) {
+        this.job = job;
+        this.jobLauncher = jobLauncher;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnSpringBatchApplication.class, args);
+    }
+
+
+    // do something at startup
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+
+            // delay
+
+            Thread.sleep(10_000);
+            jobLauncher.run(job, new JobParameters());
+
+        };
+    }
 
 }
